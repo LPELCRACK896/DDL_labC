9a9193e80475f8fce7dc4c499131eac463f0621c
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-04-09 17:35:25 -0600

    yalex file read

--- yalex_to_regex.py
+++ yalex_to_regex.py
@@ -1,91 +1,138 @@
+import re
 
-class Instruction():
-
-    def __init__(self, type, name, dicc_data, list_data) -> None:
-        self.type = type
-        self.name = name
-        self.dicc_data = dicc_data
-        self.list_data = list_data
 
 def read_yalex(yalex_file):
-    instructions = {}
-    
-    prelines = ["let", "rule"]
-    special_prelines = ["|"]
-    other_prelines = ["", " ", "\n", "\t"]
+
     waiting_first_rule = False 
     expects_rule = False
-    pending_rule = None
-    tokens = []
+    last_rule = None
+    lets = {}
     rules = {}
+
     with open(yalex_file, 'r') as file:
         i = 0
         lines = file.readlines()
         error = None
         while not error and i<len(lines):
             line = lines[i]
-            line_split = line.split(" ")
-            pref = line_split[0]
-            accepted_pref = False
-            pref_disp = None
-            j = 0
-            while not accepted_pref and j<len(line_split) and not error:
-                pref = line_split[j]
-                if not (pref==" " or pref==" " or pref=="\t"):
-                    if pref=="\n":
-                        accepted_pref = True
-                        pref_disp = "skip"
-                    elif pref=="let" or pref=="rule":
-                        accepted_pref = True
-                        pref_disp = pref
-                    else:
-                        if waiting_first_rule:
+            line = re.sub(r'(?<!["\'])[\t\n](?!["\'])', '', line)
+            line_split = re.findall(r'''(?:[^ "']|"[^"]*"|'[^']*')+''', line)
+            if len(line_split)>0:
+                pref = line_split[0]
+                accepted_pref = False
+                pref_disp = None
+                j = 0
+                while not accepted_pref and j<len(line_split) and not error:
+                    pref = line_split[j]
+                    if not (pref==" " or pref==" " or pref=="\t"):
+                        if pref=="\n":
                             accepted_pref = True
-                            pref_disp = "first_rule"
-                        elif expects_rule:
-                            if pref=="|":
+                            pref_disp = "skip"
+                        elif pref=="let" or pref=="rule":
+                            accepted_pref = True
+                            pref_disp = pref
+                        else:
+                            if waiting_first_rule:
                                 accepted_pref = True
-                                pref_disp = "extra_rule"
-                j += 1
-            
+                                pref_disp = "first_rule"
+                            elif expects_rule:
+                                if pref=="|":
+                                    accepted_pref = True
+                                    pref_disp = "extra_rule"
+                    j += 1
+            else:
+                accepted_pref = True
+                pref_disp = "skip"
+
             if not error:
                 if accepted_pref:
                     if pref_disp != "skip":
                         rest_line = line_split[j:]
                         if pref_disp == "let":
-                            
-                            print(0)
+                            if not waiting_first_rule:
+                                expects_rule = False
+                                r_line = "".join(rest_line)
+                                if "=" in r_line:
+                                    line_items = r_line.split("=")
+                                    if len(line_items)==2:
+                                        lets[line_items[0]] = line_items[1]
+                                    else:
+                                        error = f"Syntax error: Expects proper assignation in let line {i+1}: {line}"
+                                else:
+                                    error = f"Syntax error: Expects asignation on let line {i+1}: {line} "
+                                print("Finish let")
+                            else:
+                                error = f"Syntax error: Was expecting rule on ln {i+1} but got let statment instead: {line}"
                         elif pref_disp == "rule":
-                            pass
+                            waiting_first_rule = True
+                            expects_rule = True
+                            if len(rest_line)==2:
+                                waiting_first_rule = True
+                                name = rest_line[0]
+                                if rest_line[1].replace(" ", "")!="=":
+                                    error = f"Syntax error: On rule line expected \"=\" right after rule variable name ln {i+1}. Got {rest_line[1]} instead"
+                                else:
+                                    rules[name] = {}
+                                    last_rule = name
+
+                            elif len(rest_line)==3:
+                                waiting_first_rule = False
+                                name = rest_line[0]
+                                if rest_line[1].replace(" ", "")!="=":
+                                    error = f"Syntax error: On rule line expected \"=\" right after rule variable name ln {i+1}. Got {rest_line[1]} instead"
+                                else:
+                                    last_rule = name
+                                    rules[name] = {}
+                                    rule_spl = rest_line[2]
+                                    name, rule = rule_spl.split("{")
+                                    rule = "{"+rule
+                                    print("a")
+                                    rules[name][rule_name] = actual_rule
+
+                            elif len(rest_line)==4:
+                                waiting_first_rule = False
+                                name = rest_line[0]
+                                if rest_line[1].replace(" ", "")!="=":
+                                    error = f"Syntax error: On rule line expected \"=\" right after rule variable name ln {i+1}. Got {rest_line[1]} instead"
+                                else:
+                                    rules[name] = {}
+                                    last_rule = name
+                                    rule_name = rest_line[2]
+                                    actual_rule = rest_line[3]
+                                    if "{" in rule_name:
+                                        rule_spl = rule_name.split("{") 
+                                        rule_name = rule_spl[0]
+                                        actual_rule = "{"+rule_spl[1]+actual_rule
+                                    rules[name][rule_name] = actual_rule
+                                    print('a')
+                            else:
+                                error = f"Syntax error: Unexepcted number of arguments on ln {i+1} (expected 2 or 3), line: {line}"
                         elif pref_disp == "first_rule":
-                            pass
+                            waiting_first_rule = False
+                            if rest_line:
+                                content = "".join(rest_line)
+                                pref = pref.replace("\t", "")
+                                rules[last_rule][pref] = content
+                            else:
+                                error = f"Syntax error: Expected content between curly braces on ln {i+1}"
                         elif pref_disp == "extra_rule":
-                            pass
-
+                            if rest_line:
+                                r_line = "".join(rest_line)
+                                name, content = r_line.split("{")
+                                name = name.replace("\t", "")
+                                content = "{"+content
+                                if name and content:
+                                    rules[last_rule][name] = content
+                                else:
+                                    error = f"Syntax error on ln {i+1}, expects rule name and output afterwards surronded by curly braces. Got {line} "
+                        print(pref_disp)
+                        print(f"\t{line}")
                 else: 
-                    error = f"Syntaxis error: Unexpected line format ln: {i}. \"{line}\""
+                    error = f"Syntax error: Unexpected line format ln: {i+1}. \"{line}\""
             i += 1
-        if error:
-            print(error)
-
-
-def yalex_let():
-    pass
-
-def yalex_rule():
-    pass
-
-def yalex_to_regex(yalex, equivalencias_actuales):
-    forbidden_simbols = [
-        '+',
-        '*',
-        '|',
-        '?'
-    ]
-    equivalencias = equivalencias_actuales
-    for char in yalex: 
-        pass
-
+        if not error:
+            return lets, rules
+        return error
 
 if __name__ == "__main__":
     filename = "yalex.txt"
